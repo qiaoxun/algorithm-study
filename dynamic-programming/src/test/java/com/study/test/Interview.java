2adfe7f816936fcd08bbdffa31e32f26f25010c7
Joey <woqiaoxun@163.com>
2019-11-30 22:57:53 +0800

    longestValidParentheses

--- dynamic-programming/src/test/java/com/study/test/Interview.java
+++ dynamic-programming/src/test/java/com/study/test/Interview.java
@@ -3,6 +3,7 @@ package com.study.test;
 import org.junit.Test;
 
 import java.util.LinkedHashSet;
+import java.util.Stack;
 
 public class Interview {
 
@@ -25,5 +26,60 @@ public class Interview {
         System.out.println(set);
         return "";
     }
+
+    /**
+     * 有效括号
+     * 给定一个只包括 '('，')'的字符串，判断字符串是否有效。注：空字符串属于有效字符串
+     * 示例 1:
+     * 输入: "(())"
+     * 输出: true
+     *
+     *  实例 2：
+     *  输入: "())("
+     * 输出: false
+     */
+
+    @Test
+    public void test2() {
+        String str = ")(";
+//        System.out.println(isStrValid(str));
+        System.out.println(isStrValid2(str));
+    }
+
+    public boolean isStrValid(String str) {
+        int strLength = str.length();
+        char left = '(';
+        char right = ')';
+        Stack<Character> braceStack = new Stack<>();
+        for (int i = 0; i < strLength; i++) {
+            char c = str.charAt(i);
+            if (c == left) {
+                braceStack.push(c);
+            } else if (c == right) {
+                braceStack.pop();
+            }
+        }
+
+        return braceStack.size() == 0;
+    }
+
+
+    public boolean isStrValid2(String str) {
+        int strLength = str.length();
+        char left = '(';
+        char right = ')';
+        int sum = 0;
+        for (int i = 0; i < strLength; i++) {
+            char c = str.charAt(i);
+            if (c == left) {
+                sum ++;
+            } else if (c == right) {
+                sum --;
+            }
+        }
+        return sum == 0;
+    }
+
+
 
 }
