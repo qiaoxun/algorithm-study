479db1097a6919442cf6507901bae8453697dacc
Joey <woqiaoxun@163.com>
2019-12-05 23:16:44 +0800

    can jump

---
+++ dynamic-programming/src/main/java/com/studu/dp/CanJump.java
@@ -0,0 +1,14 @@
+package com.studu.dp;
+
+public class CanJump {
+
+    private boolean canJumpFromPosition(int position, int[] nums) {
+
+        if (position == nums.length - 1 || nums[position] >= nums.length - position)
+            return true;
+
+
+
+    }
+
+}
