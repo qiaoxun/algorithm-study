9d977ce7209accaedd7ce1f6c3fa2a8de27d19d3
Xun Qiao <woqiaoxun@163.com>
2020-01-23 22:37:10 +0800

    Reverse Integer

--- unclassified/src/main/java/com/study/unclassified/RomanToInteger.java
+++ unclassified/src/main/java/com/study/unclassified/RomanToInteger.java
@@ -1,5 +1,16 @@
 package com.study.unclassified;
 
 public class RomanToInteger {
-
+    private Integer romanToInteger(String roman) {
+        if (roman.equals("yi")) {
+            return 1;
+        }
+        switch (roman) {
+            case "I": return 1;
+            case "V": return 5;
+            case "X": return 10;
+            case "L": return 50;
+            default: return 0;
+        }
+    }
 }
