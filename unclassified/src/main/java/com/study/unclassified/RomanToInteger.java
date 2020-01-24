d440aa066405e62457a1f9263d3cbea062254521
Xun Qiao <woqiaoxun@163.com>
2020-01-24 23:14:12 +0800

    Roman to Integer

--- unclassified/src/main/java/com/study/unclassified/RomanToInteger.java
+++ unclassified/src/main/java/com/study/unclassified/RomanToInteger.java
@@ -1,15 +1,48 @@
 package com.study.unclassified;
 
+import org.junit.Test;
+
 public class RomanToInteger {
-    private Integer romanToInteger(String roman) {
-        if (roman.equals("yi")) {
-            return 1;
+
+    @Test
+    public void test() {
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+
+    public int romanToInt(String s) {
+        int sum = 0;
+        char[] romanChars = s.toCharArray();
+        char before = ' ';
+        for (int i = 0; i < romanChars.length; i++) {
+            char c = romanChars[i];
+            int represent = getRomanRepresent(c);
+            sum += getRomanRepresent(c);
+
+            if ((c == 'V' || c =='X') && before == 'I') {
+                sum -= 2;
+            } else if((c == 'L' || c == 'C') && before == 'X') {
+                sum -= 20;
+            } else if((c == 'D' || c == 'M') && before == 'C') {
+                sum -= 200;
+            }
+
+            before = c;
         }
-        switch (roman) {
-            case "I": return 1;
-            case "V": return 5;
-            case "X": return 10;
-            case "L": return 50;
+        return sum;
+    }
+
+    private Integer getRomanRepresent(Character romanCharacter) {
+        switch (romanCharacter) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
             default: return 0;
         }
     }
