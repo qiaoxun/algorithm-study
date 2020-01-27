1fba0a6abce7a43d86184350aa91fa8279f7e4c0
Xun Qiao <woqiaoxun@163.com>
2020-01-27 21:38:55 +0800

    Greedy

--- dynamic-programming/src/main/java/com/studu/dp/MaximumSubarray.java
+++ dynamic-programming/src/main/java/com/studu/dp/MaximumSubarray.java
@@ -2,6 +2,9 @@ package com.studu.dp;
 
 import org.junit.Test;
 
+/**
+ * https://leetcode.com/problems/maximum-subarray/submissions/
+ */
 public class MaximumSubarray {
 
     /**
@@ -12,14 +15,17 @@ public class MaximumSubarray {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         int[] nums1 = {-2,-1,-3};
         int[] nums2 = {1, -1, 5, -4, 0, 1, 2};
-        System.out.println(maxSubArray(nums));
-        System.out.println("DP nums " + maxSubArray_DP(nums));
-        System.out.println(maxSubArray(nums1));
-        System.out.println("DP nums1 " + maxSubArray_DP(nums1));
-        System.out.println("DP nums2 " + maxSubArray_DP(nums2));
-        System.out.println("DP1 nums " + maxSubArray_DP1(nums));
-        System.out.println("DP1 nums1 " + maxSubArray_DP1(nums1));
-        System.out.println("DP1 nums2 " + maxSubArray_DP1(nums2));
+//        System.out.println(maxSubArray(nums));
+//        System.out.println("DP nums " + maxSubArray_DP(nums));
+//        System.out.println(maxSubArray(nums1));
+//        System.out.println("DP nums1 " + maxSubArray_DP(nums1));
+//        System.out.println("DP nums2 " + maxSubArray_DP(nums2));
+//        System.out.println("DP1 nums " + maxSubArray_DP1(nums));
+//        System.out.println("DP1 nums1 " + maxSubArray_DP1(nums1));
+//        System.out.println("DP1 nums2 " + maxSubArray_DP1(nums2));
+        System.out.println("Greedy nums " + maxSubArray_Greedy(nums));
+        System.out.println("Greedy nums1 " + maxSubArray_Greedy(nums1));
+        System.out.println("Greedy nums2 " + maxSubArray_Greedy(nums2));
     }
 
     /**
@@ -64,4 +70,21 @@ public class MaximumSubarray {
         }
         return max;
     }
+
+    public int maxSubArray_Greedy(int[] nums) {
+        int max = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (currentSum + nums[i] > nums[i]) {
+                currentSum += nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+
+            max = max > currentSum ? max : currentSum;
+        }
+        return max;
+    }
+
 }
