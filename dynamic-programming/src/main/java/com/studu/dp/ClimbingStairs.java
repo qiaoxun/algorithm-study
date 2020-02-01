16c151d9af6a03139b6820b0d8d7cedef4056305
Xun Qiao <woqiaoxun@163.com>
2020-02-01 21:06:12 +0800

    Brute Force

--- dynamic-programming/src/main/java/com/studu/dp/ClimbingStairs.java
+++ dynamic-programming/src/main/java/com/studu/dp/ClimbingStairs.java
@@ -6,12 +6,15 @@ public class ClimbingStairs {
 
     @Test
     public void test() {
-        System.out.println(climbStairs(2));
+        System.out.println(climbStairs_DP(2));
         System.out.println(climbStairs_BF(2));
-        System.out.println(climbStairs(3));
-        System.out.println(climbStairs_BF(3));
-        System.out.println(climbStairs(4));
+        System.out.println(climbStairs1(2));
+        System.out.println(climbStairs_DP(3));
+        System.out.println(climbStairs_DP(3));
+        System.out.println(climbStairs1(3));
+        System.out.println(climbStairs_DP(4));
         System.out.println(climbStairs_BF(4));
+        System.out.println(climbStairs1(4));
     }
 
     /**
@@ -24,13 +27,30 @@ public class ClimbingStairs {
         return climbStairs_BF(n - 1) + climbStairs_BF(n - 2);
     }
 
+    /**
+     * Brute Force
+     * @param n
+     * @return
+     */
+    public int climbStairs1(int n) {
+        if (n <= 2) return n;
+        int first = 1;
+        int second = 2;
+        int sum = 0;
+        for (int i = 3; i <= n; i++) {
+            sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return sum;
+    }
 
     /**
      * DP
      * @param n
      * @return
      */
-    public int climbStairs(int n) {
+    public int climbStairs_DP(int n) {
         if (n == 1) return 1;
         int[] dp = new int[n + 1];
         dp[0] = 1;
