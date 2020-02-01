c0a00f4dec1a942a2a44fb54a60b51de1a8ebb65
Xun Qiao <woqiaoxun@163.com>
2020-02-01 19:25:58 +0800

    DP Solution

--- dynamic-programming/src/main/java/com/studu/dp/MinimumPathSum_1.java
+++ dynamic-programming/src/main/java/com/studu/dp/MinimumPathSum_1.java
@@ -27,7 +27,9 @@ public class MinimumPathSum_1 {
         int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
         int[][] grid1 = {{1},{1}};
         System.out.println(minPathSum_DP(grid));
+        System.out.println(minPathSum_DP1(grid));
         System.out.println(minPathSum_DP(grid1));
+        System.out.println(minPathSum_DP1(grid1));
     }
 
     /**
@@ -57,8 +59,6 @@ public class MinimumPathSum_1 {
         int m = grid.length;
         int n = grid[0].length;
 
-        int minSum = 0;
-
         int[][] dp = new int[m][n];
 
         dp[0][0] = grid[0][0];
@@ -80,4 +80,32 @@ public class MinimumPathSum_1 {
         return dp[m - 1][n - 1];
     }
 
+    /**
+     * DP
+     *
+     */
+    public int minPathSum_DP1(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] dp = new int[n];
+
+        dp[0] = grid[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = grid[i][j] + dp[j];
+                } else {
+                    dp[j] = grid[i][j] + Math.min(dp[j - 1], dp[j]);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
 }
