2d186e7f34c8cac2a8dc941081f2d9bfe50ac9cd
Xun Qiao <woqiaoxun@163.com>
2020-02-02 21:48:17 +0800

    Not Working

---
+++ dynamic-programming/src/main/java/com/studu/dp/DecodeWays.java
@@ -0,0 +1,31 @@
+package com.studu.dp;
+
+import org.junit.Test;
+
+public class DecodeWays {
+
+    @Test
+    public void test() {
+        System.out.println(numDecodings("0"));
+        System.out.println(numDecodings("10"));
+        System.out.println(numDecodings("270"));
+        System.out.println(numDecodings("2000"));
+    }
+
+    public int numDecodings(String s) {
+        char[] chars = s.toCharArray();
+        if (chars.length == 1) {
+            if (chars[0] > '0') return 1;
+            else return 0;
+        }
+
+        int sum = 1;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i - 1] < '3' && chars[i - 1] != '0' && chars[i] < '7' && chars[i] == '0' && i != chars.length - 1) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+}
