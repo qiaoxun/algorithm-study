a7c9412b38d768d13bec3b7f82df6ee5f0c83f39
Xun Qiao <woqiaoxun@163.com>
2020-02-07 23:45:14 +0800

    Not Working

--- dynamic-programming/src/main/java/com/studu/dp/DecodeWays.java
+++ dynamic-programming/src/main/java/com/studu/dp/DecodeWays.java
@@ -6,10 +6,23 @@ public class DecodeWays {
 
     @Test
     public void test() {
-        System.out.println(numDecodings("0"));
-        System.out.println(numDecodings("10"));
-        System.out.println(numDecodings("270"));
-        System.out.println(numDecodings("2000"));
+        System.out.println("0 " + numDecodings("0"));
+        System.out.println("00 " + numDecodings("00"));
+        System.out.println("12 " + numDecodings("12"));
+        System.out.println("010 " + numDecodings("010"));
+        System.out.println("101 " + numDecodings("101"));
+        System.out.println("10 " + numDecodings("10"));
+        System.out.println("100 " + numDecodings("100"));
+        System.out.println("270 " + numDecodings("270"));
+        System.out.println("2002 " + numDecodings("2002"));
+        System.out.println("2000 " + numDecodings("2000"));
+        System.out.println("20000 " + numDecodings("20000"));
+//        char a = 1;
+//        char b = '0';
+//        char c = ' ';
+//        System.out.println(a == b);
+//        System.out.println(a < b);
+//        System.out.println(b < c);
     }
 
     public int numDecodings(String s) {
@@ -19,11 +32,29 @@ public class DecodeWays {
             else return 0;
         }
 
+        char first = chars[0];
+        char second = chars[1];
+
+        if (first == '0') {
+            return 0;
+        }
+
         int sum = 1;
-        for (int i = 1; i < chars.length; i++) {
-            if (chars[i - 1] < '3' && chars[i - 1] != '0' && chars[i] < '7' && chars[i] == '0' && i != chars.length - 1) {
-                sum++;
+        if (first < '3' && first != '0' && second < '7' && second != '0') {
+            sum = 2;
+        }
+        for (int i = 2; i < chars.length; i++) {
+            char current = chars[i];
+            if (current == '0') {
+                if (second > '0' && second < '3') sum++;
+                else if (second == '0' || second > '6') return 0;
+            } else {
+                if ('0' < second && second < '3') {
+                    if (current < '7') sum++;
+                }
             }
+            first = second;
+            second = current;
         }
         return sum;
     }
