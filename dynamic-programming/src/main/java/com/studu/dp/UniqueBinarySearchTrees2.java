77c97514664c753480c8d0837e177b9014afbe0a
Xun Qiao <woqiaoxun@163.com>
2020-02-11 23:38:11 +0800

    UniqueBinarySearchTrees2

---
+++ dynamic-programming/src/main/java/com/studu/dp/UniqueBinarySearchTrees2.java
@@ -0,0 +1,17 @@
+package com.studu.dp;
+
+import java.util.List;
+
+public class UniqueBinarySearchTrees2 {
+    public List<TreeNode> generateTrees(int n) {
+
+        return null;
+    }
+}
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
