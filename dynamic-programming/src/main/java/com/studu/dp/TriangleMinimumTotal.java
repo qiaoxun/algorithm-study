7388b1523772b0292401757442d96c0639940d56
Xun Qiao <woqiaoxun@163.com>
2020-02-15 23:36:14 +0800

    Time Limit Exceeded

--- dynamic-programming/src/main/java/com/studu/dp/TriangleMinimumTotal.java
+++ dynamic-programming/src/main/java/com/studu/dp/TriangleMinimumTotal.java
@@ -1,5 +1,9 @@
 package com.studu.dp;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,25 +11,87 @@ import java.util.List;
  *
  */
 public class TriangleMinimumTotal {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int sum = 0;
-        for (int i = 0; i < triangle.size(); i++) {
-            List<Integer> each = triangle.get(i);
-            sum += findMinimumNum(each);
-        }
-        return sum;
+
+    @Test
+    public void test() {
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(2);
+
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(3);
+        l2.add(4);
+
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(6);
+        l3.add(5);
+        l3.add(7);
+
+        List<Integer> l4 = new ArrayList<>();
+        l4.add(4);
+        l4.add(1);
+        l4.add(8);
+        l4.add(3);
+
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(l1);
+        triangle.add(l2);
+        triangle.add(l3);
+        triangle.add(l4);
+
+
+        List<Integer> l11 = new ArrayList<>();
+        l11.add(-1);
+
+        List<Integer> l22 = new ArrayList<>();
+        l22.add(2);
+        l22.add(3);
+
+        List<Integer> l33 = new ArrayList<>();
+        l33.add(1);
+        l33.add(-1);
+        l33.add(-3);
+
+        List<List<Integer>> triangle1 = new ArrayList<>();
+        triangle1.add(l11);
+        triangle1.add(l22);
+        triangle1.add(l33);
+
+        System.out.println(minimumTotal(triangle));
+        System.out.println(minimumTotal(triangle1));
     }
 
-    public int findMinimumNum(List<Integer> list) {
-        if (list.size() == 1) {
-            return list.get(0);
-        }
-        int minimum = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            if (minimum > list.get(i)) {
-                minimum = list.get(i);
-            }
-        }
-        return minimum;
+
+    /**
+     * Brute Force
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int minimumSum = triangle.get(0).get(0);
+
+        minimumSum += findMinimumTotal(triangle, 1, 0);
+
+        return minimumSum;
     }
+
+    public int findMinimumTotal(List<List<Integer>> triangle, int start, int position) {
+        if (start == triangle.size()) return 0;
+
+        int sumA = triangle.get(start).get(position) + findMinimumTotal(triangle, start + 1, position);
+        int sumB = triangle.get(start).get(position + 1) + findMinimumTotal(triangle, start + 1, position + 1);
+
+        return sumA < sumB ? sumA : sumB;
+    }
+
+    /**
+     * DP
+     */
+    public int minimumTotal_DP(List<List<Integer>> triangle) {
+        int minimumSum = triangle.get(0).get(0);
+
+        minimumSum += findMinimumTotal(triangle, 1, 0);
+
+        return minimumSum;
+    }
+
 }
