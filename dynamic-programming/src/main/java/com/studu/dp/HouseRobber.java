95dd5a88c1f9a1397fbbd694cab8900aea0eb7f3
Xun Qiao <xun.qiao@microfocus.com>
2020-02-19 23:20:01 +0800

    DP

--- dynamic-programming/src/main/java/com/studu/dp/HouseRobber.java
+++ dynamic-programming/src/main/java/com/studu/dp/HouseRobber.java
@@ -29,11 +29,23 @@ public class HouseRobber {
         int[] nums4 = {2, 1, 1, 1, 2};
         int[] nums5 = {2, 1};
         System.out.println(rob(nums));
+        System.out.println(rob1(nums));
+        System.out.println(rob2(nums));
         System.out.println(rob(nums1));
+        System.out.println(rob1(nums1));
+        System.out.println(rob2(nums1));
         System.out.println(rob(nums2));
+        System.out.println(rob1(nums2));
+        System.out.println(rob2(nums2));
         System.out.println(rob(nums3));
+        System.out.println(rob1(nums3));
+        System.out.println(rob2(nums3));
         System.out.println(rob(nums4));
+        System.out.println(rob1(nums4));
+        System.out.println(rob2(nums4));
         System.out.println(rob(nums5));
+        System.out.println(rob1(nums5));
+        System.out.println(rob2(nums5));
     }
 
     /**
@@ -52,6 +64,36 @@ public class HouseRobber {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
         return dp[nums.length - 1];
+    }
+
+    public int rob1(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        int pre1 = Math.max(nums[0], nums[1]);
+        int pre2 = nums[0];
+        int curr = pre1;
+        for (int i = 2; i < nums.length; i++) {
+            curr = Math.max(pre1, pre2 + nums[i]);
+            pre2 = pre1;
+            pre1 = curr;
+        }
+        return curr;
+    }
+
+
+    public int rob2(int[] nums) {
+        int preMax = 0;
+        int currMax = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int temp = currMax;
+            currMax = Math.max(currMax, preMax + nums[i]);
+            preMax = temp;
+        }
+        return currMax;
     }
 
 }
