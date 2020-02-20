37b44b737f0749e890a98213878837bce1217ce4
Xun Qiao <woqiaoxun@163.com>
2020-02-20 23:35:16 +0800

    DP

--- dynamic-programming/src/main/java/com/studu/dp/HouseRobberII.java
+++ dynamic-programming/src/main/java/com/studu/dp/HouseRobberII.java
@@ -16,14 +16,25 @@ public class HouseRobberII {
     }
 
     public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
         int preMax = 0;
         int currMax = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
             int temp = currMax;
             currMax = Math.max(preMax + nums[i], currMax);
             preMax = temp;
         }
-        return currMax;
+
+        int preMax1 = 0;
+        int currMax1 = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int temp = currMax1;
+            currMax1 = Math.max(preMax1 + nums[i], currMax1);
+            preMax1 = temp;
+        }
+
+        return currMax > currMax1 ? currMax : currMax1;
     }
 
 }
