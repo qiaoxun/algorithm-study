9c3b208cfaeee467105fcba5876d045eba200af6
Xun Qiao <woqiaoxun@163.com>
2020-02-21 23:51:32 +0800

    MaximalSquare

---
+++ dynamic-programming/src/main/java/com/studu/dp/MaximalSquare.java
@@ -0,0 +1,20 @@
+package com.studu.dp;
+
+public class MaximalSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        int a = matrix.length;
+        int b = matrix[0].length;
+
+        int[][] dp = new int[a][b];
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+
+            }
+        }
+
+        return 0;
+    }
+
+}
