68c2be2bb0dd788fd63b69e5e244df9afe9a24f1
Xun Qiao <woqiaoxun@163.com>
2020-02-22 13:02:39 +0800

    DP

--- dynamic-programming/src/main/java/com/studu/dp/MaximalSquare.java
+++ dynamic-programming/src/main/java/com/studu/dp/MaximalSquare.java
@@ -1,20 +1,50 @@
 package com.studu.dp;
 
+import org.junit.Test;
+
+
 public class MaximalSquare {
 
+    @Test
+    public void test() {
+        char[] char0 = {1, 0, 1, 0, 0};
+        char[] char1 = {1, 0, 1, 1, 1};
+        char[] char2 = {1, 1, 1, 1, 1};
+        char[] char3 = {1, 0, 0, 1, 0};
+        char[][] matrix = {char0, char1, char2, char3};
+        System.out.println(maximalSquare(matrix));
+    }
+
     public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0) return 0;
         int a = matrix.length;
         int b = matrix[0].length;
 
         int[][] dp = new int[a][b];
+        int max = 0;
 
         for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-
-            }
+            dp[i][0] = matrix[i][0] == '0' ? 0 : 1;
+            max = max > dp[i][0] ? max : dp[i][0];
         }
 
-        return 0;
+        for (int i = 0; i < b; i++) {
+            dp[0][i] = matrix[0][i] == '0' ? 0 : 1;;
+            max = max > dp[0][i] ? max : dp[0][i];
+        }
+
+        for (int i = 1; i < a; i++) {
+            for (int j = 1; j < b; j++) {
+                if (matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    int miniNum = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+                    dp[i][j] = (int)Math.pow(Math.pow(miniNum, 0.5) + 1, 2);
+                    max = max > dp[i][j] ? max : dp[i][j];
+                }
+            }
+        }
+        return max;
     }
 
 }
