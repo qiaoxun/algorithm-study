2bec71bb1fe2c996f26a2493541e5182181505d8
Xun Qiao <xun.qiao@microfocus.com>
2020-03-01 23:16:08 +0800

    DP

--- unclassified/src/main/java/com/study/unclassified/PerfectSquares.java
+++ unclassified/src/main/java/com/study/unclassified/PerfectSquares.java
@@ -8,7 +8,11 @@ public class PerfectSquares {
     public void test() {
 //        System.out.println(Math.sqrt(3));
         System.out.println(numSquares(12));
+        System.out.println(numSquares1(12));
+        System.out.println(numSquares_DP(12));
         System.out.println(numSquares(17));
+        System.out.println(numSquares1(17));
+        System.out.println(numSquares_DP(17));
     }
 
     public int numSquares(int n) {
@@ -20,13 +24,12 @@ public class PerfectSquares {
         int res = Integer.MAX_VALUE;
         for (int i = 1; i * i < n; i++) {
             res = Math.min(res, numSquares(n - i * i) + 1);
-            System.out.println(i);
         }
         return res;
     }
 
     public int numSquares1(int n) {
-        int[] memo = new int[n];
+        int[] memo = new int[n + 1];
         return calc(n, memo);
     }
 
@@ -40,10 +43,21 @@ public class PerfectSquares {
 
         int res = Integer.MAX_VALUE;
         for (int i = 1; i * i < n; i++) {
-            res = Math.min(res, numSquares(n - i * i) + 1);
-            System.out.println(i);
+            res = Math.min(res, calc(n - i * i, memo) + 1);
         }
         return memo[n] = res;
+    }
+
+
+    public int numSquares_DP(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
     }
 
 }
