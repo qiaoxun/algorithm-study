2e1ac0c55aeb6aa7fa4142850f1a81c72faff8ed
Xun Qiao <woqiaoxun@163.com>
2020-03-06 23:23:46 +0800

    Range Sum Query - Immutable

---
+++ dynamic-programming/src/main/java/com/studu/dp/LongestPalindromicSubstring.java
@@ -0,0 +1,17 @@
+package com.studu.dp;
+
+public class LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i + 1; j < chars.length; j++) {
+
+            }
+
+        }
+        return "";
+    }
+
+}
