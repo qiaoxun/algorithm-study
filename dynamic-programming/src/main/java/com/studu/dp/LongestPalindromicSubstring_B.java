1cc7036a3bdaa1b8122fb8b0993ebc11eccc196a
Xun Qiao <xun.qiao@microfocus.com>
2020-03-08 15:26:22 +0800

    DP

--- dynamic-programming/src/main/java/com/studu/dp/LongestPalindromicSubstring_B.java
+++ dynamic-programming/src/main/java/com/studu/dp/LongestPalindromicSubstring_B.java
@@ -6,18 +6,18 @@ public class LongestPalindromicSubstring_B {
 
     @Test
     public void test() {
-//        String str = "abaccdcca";
-//        String str1 = "cbbd";
-//        String str2 = "babad";
-//        String str3 = "abc";
-//        String str4 = "aaaaaa";
+        String str = "abaccdcca";
+        String str1 = "cbbd";
+        String str2 = "babad";
+        String str3 = "abc";
+        String str4 = "aaaaaa";
         String str5 = "abcda";
         String str6 = "babad";
-//        System.out.println(longestPalindrome(str));
-//        System.out.println(longestPalindrome(str1));
-//        System.out.println(longestPalindrome(str2));
-//        System.out.println(longestPalindrome(str3));
-//        System.out.println(longestPalindrome(str4));
+        System.out.println(longestPalindrome(str));
+        System.out.println(longestPalindrome(str1));
+        System.out.println(longestPalindrome(str2));
+        System.out.println(longestPalindrome(str3));
+        System.out.println(longestPalindrome(str4));
         System.out.println(longestPalindrome(str5));
         System.out.println(longestPalindrome(str6));
     }
@@ -54,23 +54,40 @@ public class LongestPalindromicSubstring_B {
 
     @Test
     public void test1() {
-//        String str = "abaccdcca";
-//        String str1 = "cbbd";
-//        String str2 = "babad";
-//        String str3 = "abc";
-//        String str4 = "aaaaaa";
+        String str = "abaccdcca";
+        String str1 = "cbbd";
+        String str2 = "babad";
+        String str3 = "abc";
+        String str4 = "aaaaaa";
         String str5 = "abcda";
         String str6 = "babad";
-//        System.out.println(longestPalindrome(str));
-//        System.out.println(longestPalindrome(str1));
-//        System.out.println(longestPalindrome(str2));
-//        System.out.println(longestPalindrome(str3));
-//        System.out.println(longestPalindrome(str4));
-        System.out.println(longestPalindrome(str5));
-        System.out.println(longestPalindrome(str6));
+        System.out.println(longestPalindrome_DP(str));
+        System.out.println(longestPalindrome_DP(str1));
+        System.out.println(longestPalindrome_DP(str2));
+        System.out.println(longestPalindrome_DP(str3));
+        System.out.println(longestPalindrome_DP(str4));
+        System.out.println(longestPalindrome_DP(str5));
+        System.out.println(longestPalindrome_DP(str6));
     }
 
 
+    public String longestPalindrome_DP(String s) {
+        if (null == s || s.equals("")) return "";
+        char[] chars = s.toCharArray();
+        int[][] dp = new int[chars.length][chars.length];
 
+        String longestStr = new String(chars, 0, 1);
+
+        for (int i = 1; i < chars.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (chars[i] == chars[j] && (i - j <= 2 || dp[i - 1][j + 1] == 1)) {
+                    dp[i][j] = 1;
+                    longestStr = longestStr.length() > i - j + 1 ? longestStr : new String(chars, j, i - j + 1);
+                }
+            }
+        }
+
+        return longestStr;
+    }
 
 }
