829cb5f4554d1cf93d39ee868ee8c58e097e5374
Xun Qiao <woqiaoxun@163.com>
2020-03-18 23:03:35 +0800

    BestTimetoBuyandSellStockwithCooldown

--- dynamic-programming/src/main/java/com/studu/dp/BestTimetoBuyandSellStockwithCooldown.java
+++ dynamic-programming/src/main/java/com/studu/dp/BestTimetoBuyandSellStockwithCooldown.java
@@ -1,9 +1,16 @@
 package com.studu.dp;
 
+import org.junit.Test;
+
 /**
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
  */
 public class BestTimetoBuyandSellStockwithCooldown {
+
+    @Test
+    public void test() {
+
+    }
     public int maxProfit(int[] prices) {
 
         return 0;
