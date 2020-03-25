e9b3c08f4b5b8d5764486f0d1a033e6031f5475d
Xun Qiao <woqiaoxun@163.com>
2020-03-25 23:15:06 +0800

    BestTimetoBuyandSellStockwithCooldown

--- dynamic-programming/src/main/java/com/studu/dp/BestTimetoBuyandSellStockwithCooldown.java
+++ dynamic-programming/src/main/java/com/studu/dp/BestTimetoBuyandSellStockwithCooldown.java
@@ -13,16 +13,28 @@ public class BestTimetoBuyandSellStockwithCooldown {
 
     @Test
     public void test() {
-
+        int[] prices = {1,2,3,0,2};
+        System.out.println(maxProfit(prices));
     }
 
     public int maxProfit(int[] prices) {
-        int dp[] = new int[prices.length];
+        int dp[][] = new int[prices.length][3];
 
-        for (int i = 0; i < prices.length; i++) {
+        /**
+         *  0 - sold
+         *  1 - hold
+         *  2 - rest
+         */
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
 
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = dp[i - 1][1] + prices[i];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][2] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][0]);
         }
 
-        return 0;
+        return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][2]);
     }
 }
