f02995ee5a169cd41c426790e6aca90af07ca4fe
Xun Qiao <woqiaoxun@163.com>
2020-04-15 23:08:07 +0800

    add test

---
+++ unclassified/src/test/java/com/test/URLEncoderTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.junit.Test;
+
+import java.net.URLEncoder;
+
+public class URLEncoderTest {
+
+    @Test
+    public void test() {
+        System.out.println(URLEncoder.encode("g!t_maYun1"));
+    }
+
+}
