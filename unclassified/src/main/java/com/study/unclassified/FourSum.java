b5e4164a1fabf5ef1480332ce7e549c4ff7c50c6
Xun Qiao <woqiaoxun@163.com>
2020-04-20 22:53:00 +0800

    four sum

--- unclassified/src/main/java/com/study/unclassified/FourSum.java
+++ unclassified/src/main/java/com/study/unclassified/FourSum.java
@@ -16,12 +16,12 @@ public class FourSum {
        int[] nums3 = {2,-4,-5,-2,-3,-5,0,4,-2};
        int[] nums4 = {0,2,2,2,10,-3,-9,2,-10,-4,-9,-2,2,8,7};
 //        System.out.println(fourSum(nums, 0));
-//        System.out.println(fourSum(nums1, 0));
+        System.out.println(fourSum(nums1, 0));
 //        System.out.println(fourSum(nums1, 1));
 //        System.out.println(fourSum(nums2, 1));
 //        System.out.println(fourSum(nums3, -14));
 //        System.out.println(fourSum_bak(nums3, -14));
-        System.out.println(fourSum(nums4, 6));
+//        System.out.println(fourSum(nums4, 6));
     }
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
@@ -31,30 +31,39 @@ public class FourSum {
         List<List<Integer>> lists = new ArrayList<>();
         Arrays.sort(nums);
         int leftTarget = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             leftTarget = target - nums[i];
 
-            for (int j = 1; j < nums.length - 1; j++) {
-                if (i == j) continue;
-                int left = 0;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+//                if (i == j) continue;
+                int left = j + 1;
                 int right = nums.length - 1;
 
                 while (left < right) {
-                    if (right == i || right == j) {
-                        right--;
-                        continue;
-                    }
-                    if (left == i || left == j) {
-                        left++;
-                        continue;
-                    }
+//                    if (right == i || right == j) {
+//                        right--;
+//                        continue;
+//                    }
+//                    if (left == i || left == j) {
+//                        left++;
+//                        continue;
+//                    }
                     int sum = nums[j] + nums[left] + nums[right];
                     if (sum == leftTarget) {
                         List<Integer> resultList = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
-                        List<Integer> exsits = lists.stream().filter(eachList -> eachList.containsAll(resultList) && resultList.containsAll(eachList)).findFirst().orElse(null);
-                        if (null == exsits || exsits.size() == 0) {
+//                        List<Integer> exsits = lists.stream().filter(eachList -> eachList.containsAll(resultList) && resultList.containsAll(eachList)).findFirst().orElse(null);
+//                        if (null == exsits || exsits.size() == 0) {
                             lists.add(resultList);
-                        }
+//                        }
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
                         left++;
                         right--;
                     } else if (sum > leftTarget) {
