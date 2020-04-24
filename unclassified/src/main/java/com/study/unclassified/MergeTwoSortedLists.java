2fafde8dab56afcff03b9665292eb0822a582a56
Xun Qiao <woqiaoxun@163.com>
2020-04-24 22:48:00 +0800

    MergeTwoSortedLists

---
+++ unclassified/src/main/java/com/study/unclassified/MergeTwoSortedLists.java
@@ -0,0 +1,16 @@
+package com.study.unclassified;
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        RotateList.ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+}
