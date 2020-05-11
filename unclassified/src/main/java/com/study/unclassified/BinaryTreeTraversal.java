969725e6ede7ca23f1dd87c7de38283d9a071c5a
Xun Qiao <woqiaoxun@163.com>
2020-05-11 21:53:47 +0800

    BinaryTreeTraversal

---
+++ unclassified/src/main/java/com/study/unclassified/BinaryTreeTraversal.java
@@ -0,0 +1,47 @@
+package com.study.unclassified;
+
+import org.junit.Test;
+
+public class BinaryTreeTraversal {
+
+    @Test
+    public void testMiddleOrder() {
+        TreeNode node = new TreeNode(3);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(5);
+        TreeNode node3 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(2);
+        TreeNode node5 = new TreeNode(3);
+        node.left = node1;
+        node.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node4.right = node5;
+//        node2.left = node3;
+//        node2.right = node4;
+        middleOrder(node);
+    }
+
+    public void middleOrder(TreeNode node) {
+        if (node != null) {
+            middleOrder(node.left);
+            System.out.println(node.val);
+            middleOrder(node.right);
+        }
+    }
+
+
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
