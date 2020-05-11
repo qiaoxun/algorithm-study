d317d2269aa860bae5b1c11c2dd2ccfd69a0496a
Xun Qiao <woqiaoxun@163.com>
2020-05-11 22:01:03 +0800

    pre order

--- unclassified/src/main/java/com/study/unclassified/BinaryTreeTraversal.java
+++ unclassified/src/main/java/com/study/unclassified/BinaryTreeTraversal.java
@@ -5,6 +5,30 @@ import org.junit.Test;
 public class BinaryTreeTraversal {
 
     @Test
+    public void testPreOrder() {
+        TreeNode node = new TreeNode(3);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(5);
+        TreeNode node3 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(2);
+        TreeNode node5 = new TreeNode(3);
+        node.left = node1;
+        node.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node4.right = node5;
+        preOrder(node);
+    }
+
+    public void preOrder(TreeNode node) {
+        if (node != null) {
+            System.out.println(node.val);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    @Test
     public void testMiddleOrder() {
         TreeNode node = new TreeNode(3);
         TreeNode node1 = new TreeNode(1);
