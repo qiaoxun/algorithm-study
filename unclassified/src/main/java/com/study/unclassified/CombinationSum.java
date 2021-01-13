39f035844451de4b379a83a3110eb7701cf8792e
Joey <woqiaoxun@163.com>
2021-01-13 22:18:17 +0800

    CombinationSum

--- unclassified/src/main/java/com/study/unclassified/CombinationSum.java
+++ unclassified/src/main/java/com/study/unclassified/CombinationSum.java
@@ -2,18 +2,44 @@ package com.study.unclassified;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CombinationSum {
 
     @Test
     public void test() {
-
+        int[] candidates = { 1, 2 };
+        int target = 2;
+        System.out.println(combinationSum(candidates, target));
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates,  int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(ans, combine, candidates, target, 0);
+        return ans;
+    }
 
-        return null;
+    private void dfs(List<List<Integer>> ans, List<Integer> combine, int[] candidates, int target, int idx) {
+        // solution found
+        if (target == 0) {
+            ans.add(new ArrayList<>(combine));
+            return;
+        }
+
+        // No solution
+        if (idx == candidates.length) {
+            return;
+        }
+
+        dfs(ans, combine, candidates, target, idx + 1);
+
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(ans, combine, candidates, target - candidates[idx], idx);
+            combine.remove(combine.size() - 1);
+        }
     }
 
 }
