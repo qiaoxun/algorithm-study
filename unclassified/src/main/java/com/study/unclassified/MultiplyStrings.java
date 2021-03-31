f832fba9a2042547b07b91b652684ce366b6edb2
Joey <woqiaoxun@163.com>
2021-03-31 22:03:54 +0800

    Multiply String

--- unclassified/src/main/java/com/study/unclassified/MultiplyStrings.java
+++ unclassified/src/main/java/com/study/unclassified/MultiplyStrings.java
@@ -2,16 +2,51 @@ package com.study.unclassified;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class MultiplyStrings {
 
     @Test
     public void test() {
-        String num1 = "123";
-        String num2 = "456";
+        String num1 = "99";
+        String num2 = "99";
         System.out.println(multiply(num1, num2));
     }
 
     public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int len1 = num1.length(), len2 = num2.length();
+
+        int[] ansArr = new int[len1 + len2];
+
+        for (int i = len1 - 1; i >= 0; i--) {
+            int x = num1.charAt(i) - '0';
+            for (int j = len2 - 1; j >= 0; j--) {
+                int y = num2.charAt(j) - '0';
+                int result = x * y;
+                ansArr[i + j + 1] += result;
+            }
+        }
+
+        StringBuffer resultSb = new StringBuffer();
+
+        for (int i = ansArr.length - 1; i > 0; i--) {
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] = ansArr[i] % 10;
+            resultSb.append(ansArr[i]);
+        }
+
+        if (ansArr[0] != 0) {
+            resultSb.append(ansArr[0]);
+        }
+
+        return resultSb.reverse().toString();
+    }
+
+
+    public String multiply1(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
