250c1f8587c649da20520268267a6ab068b7a810
Joey <woqiaoxun@163.com>
2021-05-17 23:07:48 +0800

    Permutations

---
+++ unclassified/src/main/java/com/study/unclassified/Permutations.java
@@ -0,0 +1,23 @@
+package com.study.unclassified;
+
+import java.util.List;
+
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+            }
+        }
+
+        return null;
+    }
+
+    private void helper() {
+
+    }
+
+
+}
