e346c1dcb93388e05f7ddb5eb5aeb2cceb72b138
Xun Qiao <woqiaoxun@163.com>
2021-12-20 14:55:11 +0800

    Permutations using dfs

--- unclassified/src/main/java/com/study/unclassified/Permutations.java
+++ unclassified/src/main/java/com/study/unclassified/Permutations.java
@@ -1,25 +1,46 @@
 package com.study.unclassified;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Permutations {
 
-    public List<List<Integer>> permute(int[] nums) {
+    public static void main(String[] args) {
+        Permutations permutations = new Permutations();
 
-        List<int[]> list = new ArrayList<>();
-        helper(nums, new int[nums.length], 0, list);
-
-        return null;
+        List<List<Integer>> result = permutations.permute(new int[] {1, 2, 3});
+        result.forEach(each -> {
+            System.out.println(Arrays.toString(each.toArray()));
+        });
     }
 
-    private int[] helper(int[] nums, int[] singleResult, int index, List<int[]> result) {
-        for (int i = index; i < nums.length; i++) {
-            singleResult[index] = nums[i];
-            helper(nums, singleResult, index + 1);
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        int len = nums.length;
+        int depth = 0;
+        Deque<Integer> stack = new ArrayDeque<>(len);
+        boolean[] used = new boolean[len];
+        dfs(nums, len, depth, stack, used, list);
+
+        return list;
+    }
+
+    private void dfs(int[] nums, int len, int depth, Deque<Integer> stack, boolean[] used, List<List<Integer>> result) {
+        if (len == depth) {
+            result.add(new ArrayList(stack));
+            return;
         }
-        return result;
+
+        for (int i = 0; i < len; i++) {
+            if (used[i]) {
+                continue;
+            }
+            stack.add(nums[i]);
+            used[i] = true;
+            dfs(nums, len, depth + 1, stack, used, result);
+
+            stack.removeLast();
+            used[i] = false;
+        }
     }
 
 
