a5be28c2c1681a82e3848353da0acb459f1ab7a0
Xun Qiao <woqiaoxun@163.com>
2022-01-01 15:09:46 +0800

    PermutationsII

--- unclassified/src/main/java/com/study/unclassified/PermutationsII.java
+++ unclassified/src/main/java/com/study/unclassified/PermutationsII.java
@@ -1,16 +1,42 @@
 package com.study.unclassified;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PermutationsII {
-    public List<List<Integer>> permuteUnique(int[] nums) {
 
-        return null;
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+        List<List<Integer>> result = new PermutationsII().permuteUnique(nums);
+        result.forEach(node -> {
+            System.out.println(node);
+        });
     }
 
-    public void dfs(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
 
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] used = new boolean[nums.length];
+        List<Integer> current = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(nums, used, current, result);
+        return result;
+    }
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            current.add(nums[i]);
+            used[i] = true;
+            dfs(nums, used, current, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
         }
     }
 
