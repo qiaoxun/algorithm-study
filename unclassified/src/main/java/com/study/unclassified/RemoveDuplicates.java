5f2ba5d4784bdf76df893a848783e52518b8e812
Joey <woqiaoxun@163.com>
2022-02-10 23:31:40 +0800

    remove duplicates

---
+++ unclassified/src/main/java/com/study/unclassified/RemoveDuplicates.java
@@ -0,0 +1,45 @@
+package com.study.unclassified;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int count = removeDuplicates.removeDuplicates(nums);
+        System.out.println(count);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        if (null == nums || nums.length == 0) return 0;
+        if (nums.length == 1) return 1;
+        int tmp = nums[0];
+        int count = 1;
+
+        int length = nums.length;
+
+        for (int i = 1; i < length; i++) {
+            if (nums[i] == tmp) {
+                moveForward(nums, i, length);
+                i--;
+                length --;
+            } else {
+                tmp = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void moveForward(int[] nums, int index, int length) {
+        for (int i = index; i < length; i++) {
+            if (i == nums.length - 1) {
+                nums[i] = 0;
+                break;
+            }
+            nums[i] = nums[i + 1];
+        }
+    }
+}
