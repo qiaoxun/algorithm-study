1a8cf0526bdad2a33ed8f4318b37e04e44b59878
Joey <woqiaoxun@163.com>
2022-03-29 09:14:36 +0800

    max heapify

--- unclassified/src/main/java/com/study/unclassified/MaxHeap.java
+++ unclassified/src/main/java/com/study/unclassified/MaxHeap.java
@@ -1,13 +1,37 @@
 package com.study.unclassified;
 
+import java.util.Arrays;
+
 public class MaxHeap {
 
     public static void main(String[] args) {
+        int arr[] = {5, 4, 7, 3};
+        MaxHeap maxHeap = new MaxHeap();
+        maxHeap.maxHeapify(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public void maxHeapify(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int parentIndex = findParent(i, array);
+            if (array[i] > array[parentIndex]) {
+                swap(i, parentIndex, array);
+            }
+        }
+    }
+
+    public void swap(int pos0, int pos1, int[] array) {
+        int temp = array[pos0];
+        array[pos0] = array[pos1];
+        array[pos1] = temp;
+    }
+
+    private Integer findParent(int i, int[] array) {
+        return (i - 1) / 2;
     }
 
     private Integer findLeftChild(int i, int[] array) {
-        int index = 2 * (i + 1);
+        int index = 2 * i;
         if (array.length > index) {
             return array[index];
         }
@@ -15,7 +39,7 @@ public class MaxHeap {
     }
 
     private Integer findRightChild(int i, int[] array) {
-        int index = 2 * (i + 1) + 1;
+        int index = 2 * i + 1;
         if (array.length > index) {
             return array[index];
         }
