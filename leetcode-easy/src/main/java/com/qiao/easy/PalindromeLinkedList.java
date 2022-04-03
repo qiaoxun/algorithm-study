08aee246a652f905bbfc42da7f22dda1eedb4807
Joey <woqiaoxun@163.com>
2022-04-03 14:41:07 +0800

    PalindromeLinkedList

--- leetcode-easy/src/main/java/com/qiao/easy/PalindromeLinkedList.java
+++ leetcode-easy/src/main/java/com/qiao/easy/PalindromeLinkedList.java
@@ -4,10 +4,44 @@ package com.qiao.easy;
  * https://leetcode.com/problems/palindrome-linked-list/
  */
 public class PalindromeLinkedList {
-    public boolean isPalindrome(ListNode head) {
 
-        return false;
+    public static void main(String[] args) {
+        PalindromeLinkedList p = new PalindromeLinkedList();
+
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(2);
+        ListNode listNode4 = new ListNode(1);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+
+        System.out.println(p.isPalindrome(listNode1));
     }
+
+    private ListNode currentNode;
+
+    public boolean isPalindrome(ListNode head) {
+        currentNode = head;
+        return recursive(head);
+    }
+
+    public boolean recursive(ListNode node) {
+        if (node != null) {
+            boolean result = recursive(node.next);
+            if (!result) {
+                return false;
+            }
+            if (currentNode.val == node.val) {
+                currentNode = currentNode.next;
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
 }
 
 class ListNode {
