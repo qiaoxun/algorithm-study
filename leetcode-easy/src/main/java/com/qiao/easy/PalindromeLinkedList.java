47a276a8c4319e0a4e851073005383f899024740
Joey <woqiaoxun@163.com>
2022-04-04 11:30:20 +0800

    PalindromeLinkedList

--- leetcode-easy/src/main/java/com/qiao/easy/PalindromeLinkedList.java
+++ leetcode-easy/src/main/java/com/qiao/easy/PalindromeLinkedList.java
@@ -1,5 +1,7 @@
 package com.qiao.easy;
 
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/palindrome-linked-list/
  */
@@ -11,11 +13,15 @@ public class PalindromeLinkedList {
         ListNode listNode1 = new ListNode(1);
         ListNode listNode2 = new ListNode(2);
         ListNode listNode3 = new ListNode(2);
-        ListNode listNode4 = new ListNode(1);
+        ListNode listNode4 = new ListNode(2);
+        ListNode listNode5 = new ListNode(2);
+        ListNode listNode6 = new ListNode(1);
 
         listNode1.next = listNode2;
         listNode2.next = listNode3;
         listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
 
         System.out.println(p.isPalindrome(listNode1));
     }
@@ -24,7 +30,9 @@ public class PalindromeLinkedList {
 
     public boolean isPalindrome(ListNode head) {
         currentNode = head;
-        return recursive(head);
+        return slowQuickSolution(head);
+//        return recursive(head);
+//        return false;
     }
 
     public boolean recursive(ListNode node) {
@@ -40,6 +48,41 @@ public class PalindromeLinkedList {
             return false;
         }
         return true;
+    }
+
+    public boolean slowQuickSolution(ListNode head) {
+        ListNode quickNode = head;
+        ListNode slowNode = head;
+
+        while (quickNode != null && quickNode.next != null && quickNode.next.next != null) {
+            slowNode = slowNode.next;
+            quickNode = quickNode.next.next;
+        }
+
+        ListNode lastNode = reverseSecondHalfPart(slowNode.next);
+        ListNode firstNode = head;
+
+        while (null != firstNode && null != lastNode) {
+            if (firstNode.val == lastNode.val) {
+                firstNode = firstNode.next;
+                lastNode = lastNode.next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public ListNode reverseSecondHalfPart(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        while (null != current) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
     }
 
 }
