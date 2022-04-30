a1a2be764cf2720bb467e51c1b405ea9c82c3f8a
Joey <woqiaoxun@163.com>
2022-04-30 21:05:15 +0800

    EvaluateDivision

---
+++ leetcode-medium/src/main/java/com/qiao/EvaluateDivision.java
@@ -0,0 +1,14 @@
+package com.qiao;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/evaluate-division/
+ */
+public class EvaluateDivision {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return null;
+    }
+
+}
